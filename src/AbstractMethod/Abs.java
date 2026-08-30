206e2df5a3e96a616453474e8b3f97b73c540b86
Sachin11p12 <sachin11p12@gmail.com>
2026-08-30 22:20:05 +0530

    Example of Abstract method

--- src/AbstractMethod/Abs.java
+++ src/AbstractMethod/Abs.java
@@ -1,5 +1,28 @@
 package AbstractMethod;
 
-public class Abs {
+abstract class Animal {
+    // Abstract method
+    abstract void sound();
+    // Normal method
+    void eat() {
+        System.out.println("Animal is eating");
+    }
+}
+class Dog extends Animal {
 
+    // Implementing abstract method
+    @Override
+    void sound() {
+        System.out.println("Dog says: Bark");
+    }
+}
+
+public class Abs {
+    public static void main(String[] args) {
+
+        Dog dog = new Dog();
+
+        dog.sound();
+        dog.eat();
+    }
 }
