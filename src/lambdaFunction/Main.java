2029efd0cd3e69055e038b81c77205531c2a5c0e
Sachin11p12 <sachin11p12@gmail.com>
2026-08-31 23:09:15 +0530

    Create thread using lambda expression !

--- src/lambdaFunction/Main.java
+++ src/lambdaFunction/Main.java
@@ -2,9 +2,16 @@ package lambdaFunction;
 
 public class Main {
     public static void main(String[] args) {
-        Employee emp = () -> "software engineer !"; // lambda expression
-        System.out.println(emp.getName());
-        Employee editor = () -> "Editor Hai !";
-        System.out.println(editor.getName());
+//        Employee emp = () -> "software engineer !"; // lambda expression
+//        System.out.println(emp.getName());
+//        Employee editor = () -> "EDITOR Hai !";
+//        System.out.println(editor.getName());
+        Runnable runnable = () -> {
+            for (int i = 1; i <=10 ; i++) {
+                System.out.println("Hello "+ i);
+            }
+        };
+        Thread childthread = new Thread(runnable);
+        childthread.run();
     }
 }
