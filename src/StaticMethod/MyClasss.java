c151dab895ec91502e0ab38d0aa26d72e7b1b8ff
Sachin11p12 <sachin11p12@gmail.com>
2026-08-31 22:36:22 +0530

    Main method under the interface only in Java 8

---
+++ src/StaticMethod/MyClasss.java
@@ -0,0 +1,7 @@
+package StaticMethod;
+
+public class MyClasss {
+    public static void main(String[] args) {
+        System.out.println("Hello ");
+    }
+}
---
+++ src/StaticMethod/MyInterface.java
@@ -0,0 +1,8 @@
+package StaticMethod;
+
+//We can write main method under the interface only in Java 8
+public interface MyInterface {
+    public static void main(String[] args) {
+        System.out.println("Hello sachin!  ");
+    }
+}
