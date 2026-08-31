0269e6403a86286e0c9d097abd6c8966b134622e
Sachin11p12 <sachin11p12@gmail.com>
2026-08-31 22:42:28 +0530

    Lambda Function Example

---
+++ src/lambdaFunction/Employee.java
@@ -0,0 +1,5 @@
+package lambdaFunction;
+
+public interface Employee {
+    String getName();
+}
---
+++ src/lambdaFunction/SoftwareEngineer.java
@@ -0,0 +1,8 @@
+package lambdaFunction;
+
+public class SoftwareEngineer implements Employee{
+    @Override
+    public String getName(){
+        return "Software Engineer !";
+    }
+}
