71e3a158123310d33bf6c0a7b953f378c17f02fa
Sachin11p12 <sachin11p12@gmail.com>
2026-08-31 22:48:45 +0530

    Implementattion of lambda expression !

--- src/lambdaFunction/Main.java
+++ src/lambdaFunction/Main.java
@@ -2,6 +2,7 @@ package lambdaFunction;
 
 public class Main {
     public static void main(String[] args) {
-
+        Employee emp = () -> "software engineer !"; // lambda expression
+        System.out.println(emp.getName());
     }
 }
