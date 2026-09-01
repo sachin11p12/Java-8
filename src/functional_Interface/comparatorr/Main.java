4b60cd06da18e73a731c74059e5a38e538aa4d0a
Sachin11p12 <sachin11p12@gmail.com>
2026-09-01 22:43:40 +0530

    what is comparator

---
+++ src/functional_Interface/comparatorr/Main.java
@@ -0,0 +1,18 @@
+package functional_Interface.comparatorr;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+// Comparator is also a functional interface Used to define custom sorting logic .
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        list.add(2);
+        list.add(20);
+        list.add(13);
+        list.add(34);
+        Collections.sort(list);
+        System.out.println(list);
+    }
+}
