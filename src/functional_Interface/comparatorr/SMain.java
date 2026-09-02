08d9058800215f58b801ba8ecee63c49cbb40005
Sachin11p12 <sachin11p12@gmail.com>
2026-09-02 23:14:18 +0530

    Using trreset to understand how treeset keeps elements in order internally using comparator

---
+++ src/functional_Interface/comparatorr/SMain.java
@@ -0,0 +1,15 @@
+package functional_Interface.comparatorr;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+public class SMain {
+    public static void main(String[] args) {
+        Set<Integer> s = new TreeSet<>();  // set is a Interface like hash set and treeSet, TreeSet keeps element in orders.
+        s.add(22);
+        s.add(1);
+        s.add(24);
+        s.add(8);
+        System.out.println("Before Manual sorting " + s);
+    }
+}
