5422bcba32a05b23e49aa04f228594bc3b6e9f08
Sachin11p12 <sachin11p12@gmail.com>
2026-09-06 17:08:48 +0530

    Used Negate Logic with predicate

--- src/predicate/SecMainn.java
+++ src/predicate/SecMainn.java
@@ -6,10 +6,14 @@ public class SecMainn {
     public static void main(String[] args) {
         Predicate<String> startWithS = x -> x.toLowerCase().charAt(0) == 's';
         Predicate<String> endWithN = x -> x.toLowerCase().charAt(x.length()-1) == 'n';
+        // -and
 //        Predicate<String> and = startWithS.and(endWithN);
 //        System.out.println(and.test("Sachin"));
-        Predicate<String> or = startWithS.or(endWithN);
-        System.out.println(or.test("Nitin"));
+        // - or
+//        Predicate<String> or = startWithS.or(endWithN);
+//        System.out.println(or.test("Nitin"));
+        // - negate
+        System.out.println(startWithS.negate().test("aachin"));
 
     }
 }
