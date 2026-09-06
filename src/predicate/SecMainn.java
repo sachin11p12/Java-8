e44ffcd54c6de82a5c0414e931a8ab92ab1f45d8
Sachin11p12 <sachin11p12@gmail.com>
2026-09-06 16:56:26 +0530

    Used Or Logic with predicate

--- src/predicate/SecMainn.java
+++ src/predicate/SecMainn.java
@@ -6,8 +6,10 @@ public class SecMainn {
     public static void main(String[] args) {
         Predicate<String> startWithS = x -> x.toLowerCase().charAt(0) == 's';
         Predicate<String> endWithN = x -> x.toLowerCase().charAt(x.length()-1) == 'n';
-        Predicate<String> and = startWithS.and(endWithN);
-        System.out.println(and.test("Sachin"));
+//        Predicate<String> and = startWithS.and(endWithN);
+//        System.out.println(and.test("Sachin"));
+        Predicate<String> or = startWithS.or(endWithN);
+        System.out.println(or.test("Nitin"));
 
     }
 }
