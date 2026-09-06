ce35edc0c06d20000aed731050ed8f3eab4c5a0b
Sachin11p12 <sachin11p12@gmail.com>
2026-09-06 16:34:57 +0530

    understand how predicate can used instead of normal logic

--- src/predicate/Main.java
+++ src/predicate/Main.java
@@ -1,11 +1,28 @@
 package predicate;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.function.Predicate;
 
 public class Main {
     public static void main(String[] args) {
     // predicate is boolean value function
-        Predicate<Integer> salaryGreaterThanLac = x -> x > 10000;
-        System.out.println(salaryGreaterThanLac.test(1000000));
+//        Predicate<Integer> salaryGreaterThanLac = x -> x > 10000;
+//        System.out.println(salaryGreaterThanLac.test(1000000));
+
+        List<Integer> numbers = Arrays.asList(1,2,3,4,5,6,7,8,9);
+        int sum = numbers.stream().filter(n -> n % 2 == 0).mapToInt(n -> n).sum();
+
+        // above are  predicate which was written in normal login in below
+//        int sum2 = 0;
+//        for (int number : numbers){
+//            if (number%2==0){
+//                sum2 += number;
+//            }
+//        }
+
+
+//        Predicate<Integer> predicate = x -> x > 10000;
     }
 }
