ab18af491a3fc985ea1cb6fada3395d8d354cf3a
Sachin11p12 <sachin11p12@gmail.com>
2026-09-06 16:39:26 +0530

    Print  Even with predicate logic

--- src/predicate/Main.java
+++ src/predicate/Main.java
@@ -11,8 +11,8 @@ public class Main {
 //        Predicate<Integer> salaryGreaterThanLac = x -> x > 10000;
 //        System.out.println(salaryGreaterThanLac.test(1000000));
 
-        List<Integer> numbers = Arrays.asList(1,2,3,4,5,6,7,8,9);
-        int sum = numbers.stream().filter(n -> n % 2 == 0).mapToInt(n -> n).sum();
+//        List<Integer> numbers = Arrays.asList(1,2,3,4,5,6,7,8,9);
+//        int sum = numbers.stream().filter(n -> n % 2 == 0).mapToInt(n -> n).sum();
 
         // above are  predicate which was written in normal login in below
 //        int sum2 = 0;
@@ -24,5 +24,13 @@ public class Main {
 
 
 //        Predicate<Integer> predicate = x -> x > 10000;
+
+        Predicate<Integer> isEven = x -> x % 2 == 0;  //test method
+        List<Integer> numbers = Arrays.asList(1,2,3,4,5,6,7,8,9);
+        for (Integer i : numbers){
+            if (isEven.test(i)){
+                System.out.println(i);
+            }
+        }
     }
 }
