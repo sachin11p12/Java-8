adf02c99527d76912a6069ef7235d3ec1bcd6b29
Sachin11p12 <sachin11p12@gmail.com>
2026-09-07 22:21:13 +0530

    isEqual in predicate

---
+++ src/predicate/IsEqualP.java
@@ -0,0 +1,39 @@
+package predicate;
+
+import java.util.function.Predicate;
+
+public class IsEqualP {
+    public static void main(String[] args) {
+        Predicate<String> startW_S = x -> x.toLowerCase().charAt(0) == 's';
+        Predicate<String> endW_N = x -> x.toLowerCase().charAt(x.length()-1) == 'n';
+        Student s1 = new Student("Sachin", 1);
+        Student s2 = new Student("Ram", 2);
+    }
+
+
+    public static class Student{
+        public Student(String name, int id) {
+            this.name = name;
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        String name;
+        int id;
+    }
+}
