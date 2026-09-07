cc7e637d92eba391c40dfd931cf2967471909a3a
Sachin11p12 <sachin11p12@gmail.com>
2026-09-07 22:33:19 +0530

    fetch 0 to 4 substring by using Function apply

--- src/predicate/Apply_P.java
+++ src/predicate/Apply_P.java
@@ -5,6 +5,11 @@ import java.util.function.Function;
 public class Apply_P {
     public static void main(String[] args) {
         Function<String, Integer> function = x -> x.length();
-        System.out.println(function.apply("Sachin tiwari"));
+//        System.out.println(function.apply("Sachin tiwari"));
+        Function<String, String> function1 = s -> s.substring(0, 4);
+
+        System.out.println(function1.apply("Sachin"));
+
+
     }
 }
