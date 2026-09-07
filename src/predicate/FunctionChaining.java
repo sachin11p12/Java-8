84d9955824b6edab94ebc3cfa3cdefe8660f9afc
Sachin11p12 <sachin11p12@gmail.com>
2026-09-07 22:51:18 +0530

    Used function in function then apply method  to convert to uppercase the n substring

--- src/predicate/FunctionChaining.java
+++ src/predicate/FunctionChaining.java
@@ -4,7 +4,10 @@ import java.util.function.Function;
 
 public class FunctionChaining {
     public static void main(String[] args) {
-        Function<String, Integer> function1 = x -> x.length();
+        Function<String, String> function1 = s -> s.toUpperCase();
         Function<String, String> function2 = s -> s.substring(0, 3);
+
+        Function<String, String> stringStringFunction = function1.andThen(function2);
+        System.out.println(stringStringFunction.apply("Sachin"));
     }
 }
