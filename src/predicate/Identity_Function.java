6742487415f12481aa147e41e3bbbbf1dfe74c24
Sachin11p12 <sachin11p12@gmail.com>
2026-09-08 22:18:18 +0530

    understand how identity function work give string and it return same

--- src/predicate/Identity_Function.java
+++ src/predicate/Identity_Function.java
@@ -4,9 +4,12 @@ import java.util.function.Function;
 
 public class Identity_Function {
     public static void main(String[] args) {
-        Function<Integer, Integer> function3 = x -> 2*x;
-        Function<Integer, Integer> function4 = x -> x*x*x;
-        System.out.println(function3.andThen(function4).apply(3));
-        System.out.println(function4.andThen(function3).apply(3));
+//        Function<Integer, Integer> function3 = x -> 2*x;
+//        Function<Integer, Integer> function4 = x -> x*x*x;
+//        System.out.println(function3.andThen(function4).apply(3));
+//        System.out.println(function4.andThen(function3).apply(3));
+
+        Function<String, String> identityFunction = Function.identity();
+        System.out.println(identityFunction.apply("Sachin"));
     }
 }
