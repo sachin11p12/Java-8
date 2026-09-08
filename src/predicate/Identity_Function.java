808331e778218191dbecf3e0473a4e14ba2b4412
Sachin11p12 <sachin11p12@gmail.com>
2026-09-08 22:15:46 +0530

    create 2 new function to understand How Identity work in function

--- src/predicate/Identity_Function.java
+++ src/predicate/Identity_Function.java
@@ -7,5 +7,6 @@ public class Identity_Function {
         Function<Integer, Integer> function3 = x -> 2*x;
         Function<Integer, Integer> function4 = x -> x*x*x;
         System.out.println(function3.andThen(function4).apply(3));
+        System.out.println(function4.andThen(function3).apply(3));
     }
 }
