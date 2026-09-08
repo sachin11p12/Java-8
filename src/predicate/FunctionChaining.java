caffd90aa5a368a6cf9c179ae00a085f6c833b9c
Sachin11p12 <sachin11p12@gmail.com>
2026-09-08 22:05:21 +0530

    use compose to understand its working

--- src/predicate/FunctionChaining.java
+++ src/predicate/FunctionChaining.java
@@ -14,6 +14,8 @@ public class FunctionChaining {
         Function<Integer, Integer> function1 = x -> 2*x;
         Function<Integer, Integer> function2 = x -> x*x*x;
         System.out.println(function1.andThen(function2).apply(3));
-        System.out.println(function2.andThen(function1).apply(3));
+//        System.out.println(function2.andThen(function1).apply(3));
+
+        System.out.println(function1.compose(function2).apply(3));
     }
 }
