197d3d9632bf0cec2d802ffbfbede667fc899718
Sachin11p12 <sachin11p12@gmail.com>
2026-09-08 22:00:31 +0530

    practice Function andThen composition

--- src/predicate/FunctionChaining.java
+++ src/predicate/FunctionChaining.java
@@ -4,11 +4,16 @@ import java.util.function.Function;
 
 public class FunctionChaining {
     public static void main(String[] args) {
-        Function<String, String> function1 = s -> s.toUpperCase();
-        Function<String, String> function2 = s -> s.substring(0, 3);
+//        Function<String, String> function1 = s -> s.toUpperCase();
+//        Function<String, String> function2 = s -> s.substring(0, 3);
 
 //        Function<String, String> stringStringFunction = function1.andThen(function2);
-        System.out.println(function1.andThen(function2).apply("Sachin"));
+//        System.out.println(function1.andThen(function2).apply("Sachin"));
 //        System.out.println(stringStringFunction.apply("Sachin"));
+
+        Function<Integer, Integer> function1 = x -> 2*x;
+        Function<Integer, Integer> function2 = x -> x*x*x;
+        System.out.println(function1.andThen(function2).apply(3));
+        System.out.println(function2.andThen(function1).apply(3));
     }
 }
