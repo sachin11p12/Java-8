d1ffe72a87fd58b20279144619eebad7f311caf1
Sachin11p12 <sachin11p12@gmail.com>
2026-09-09 22:06:54 +0530

    use List under consumer

--- src/predicate/Consumer_Interface.java
+++ src/predicate/Consumer_Interface.java
@@ -1,10 +1,19 @@
 package predicate;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.function.Consumer;
 
 public class Consumer_Interface {
     public static void main(String[] args) {
-        Consumer<String> consumer = s -> System.out.println(s);
-        consumer.accept("Sachin");
+        Consumer<String> consumer = s -> System.out.println(s); // it is save not return anything
+
+        Consumer<List<Integer>> listConsumer = li -> {
+            for (Integer i : li){
+                System.out.println(i+100);
+            }
+        };
+//        consumer.accept("Sachin");
+        listConsumer.accept(Arrays.asList(1,2,4,5));
     }
 }
