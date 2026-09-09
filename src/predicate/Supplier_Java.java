61df434835a5483f27dd7f677cdba45515d4fbad
Sachin11p12 <sachin11p12@gmail.com>
2026-09-09 22:16:45 +0530

    Use Predicate , Function , Consumer and Supplier to understand its working as differently

--- src/predicate/Supplier_Java.java
+++ src/predicate/Supplier_Java.java
@@ -1,10 +1,19 @@
 package predicate;
 
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.function.Supplier;
 
 public class Supplier_Java {
     public static void main(String[] args) {
-        Supplier<Integer> supplier = () -> 1;
-        System.out.println(supplier.get());
+//        Supplier<Integer> supplier = () -> 1;
+//        System.out.println(supplier.get());
+
+        Predicate<Integer> predicate = x -> x%2 == 0;
+        Function<Integer, Integer> function = x -> x*x;
+        Consumer<Integer> consumer = x -> System.out.println(x);
+        Supplier<Integer> supplier = () -> 100;
+
     }
 }
