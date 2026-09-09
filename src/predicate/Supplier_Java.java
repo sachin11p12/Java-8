08777dad454793ec5d0cecd93abe7b46cba88546
Sachin11p12 <sachin11p12@gmail.com>
2026-09-09 22:13:55 +0530

    We call supplier with only get method and it is not let any parameter

--- src/predicate/Supplier_Java.java
+++ src/predicate/Supplier_Java.java
@@ -4,6 +4,7 @@ import java.util.function.Supplier;
 
 public class Supplier_Java {
     public static void main(String[] args) {
-        Supplier
+        Supplier<Integer> supplier = () -> 1;
+        System.out.println(supplier.get());
     }
 }
