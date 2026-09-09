47726a738bfad90915e65b4b8b253ba62a8a224d
Sachin11p12 <sachin11p12@gmail.com>
2026-09-09 22:04:00 +0530

    Write consumer program to understand its working

--- src/predicate/Consumer_Interface.java
+++ src/predicate/Consumer_Interface.java
@@ -1,7 +1,10 @@
 package predicate;
 
+import java.util.function.Consumer;
+
 public class Consumer_Interface {
     public static void main(String[] args) {
-
+        Consumer<String> consumer = s -> System.out.println(s);
+        consumer.accept("Sachin");
     }
 }
