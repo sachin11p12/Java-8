82fee69ef7b51e5183d2be020da32e2c2e2595c0
Sachin11p12 <sachin11p12@gmail.com>
2026-09-09 22:24:19 +0530

    testing how Bipredecate works but somehow it not running

--- src/predicate/BiPredicate.java
+++ src/predicate/BiPredicate.java
@@ -1,7 +1,11 @@
 package predicate;
+import java.util.function.*;
+java.util.function.BiPredicate;
 
 public class BiPredicate {
     public static void main(String[] args) {
-        // it is used
+        // it is used to let 2 numbers, string that give boolean output
+        BiPredicate<Integer, Integer> biPredicate = (x,y) -> x%2 == 0  &&  y%2 == 0;
+        System.out.println(biPredicate.test(2,4));
     }
 }
