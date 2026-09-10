11af8040b732ce6320c4ada2f42af758a6c7d43c
Sachin11p12 <sachin11p12@gmail.com>
2026-09-10 21:43:58 +0530

    Implement Biconsumer

--- src/predicate/Bi_Consumer_java.java
+++ src/predicate/Bi_Consumer_java.java
@@ -1,12 +1,18 @@
 package predicate;
 
+import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 
 public class Bi_Consumer_java {
     public static void main(String[] args) {
-        Consumer<Integer> consumer = (x) -> {
-            System.out.println(x);
+//        Consumer<Integer> consumer = (x) -> {
+//            System.out.println(x);
+//        };
+//        consumer.accept(499);
+
+        BiConsumer<Integer, Integer> biConsumer = (x,y) -> {
+            System.out.println(x+y);
         };
-        consumer.accept(499);
+        biConsumer.accept(2,90);
     }
 }
