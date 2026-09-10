b6a283100cc991125b7ce73c4d534a90ae890870
Sachin11p12 <sachin11p12@gmail.com>
2026-09-10 21:06:44 +0530

    I accidently named BiPredicate so it was not implementing function interface

--- src/predicate/BiPredicate_Java.java
+++ src/predicate/BiPredicate_Java.java
@@ -1,12 +1,10 @@
 package predicate;
 import java.util.function.*;
-java.util.function.BiPredicate;
 
-public class BiPredicate {
+public class BiPredicate_Java {
     public static void main(String[] args) {
         // it is used to let 2 numbers, string that give boolean output
-        BiPredicate<Integer, Integer> biPredicate = (x,y) -> x%2 == 0  &&  y%2 == 0;
+        BiPredicate<Integer, Integer> biPredicate = (x, y) -> x%2 == 0  &&  y%2 == 0;
         System.out.println(biPredicate.test(2,4));
     }
-
 }
