a5568ae822ac2c5ec24fbec30dafef3daf5f0e7e
Sachin11p12 <sachin11p12@gmail.com>
2026-09-10 21:39:16 +0530

    Understand andThen Method

--- src/predicate/Bi_Function_java.java
+++ src/predicate/Bi_Function_java.java
@@ -6,6 +6,6 @@ public class Bi_Function_java {
     public static void main(String[] args) {
         BiFunction<String, String, Integer> biFunction = (x, y) -> x.length() + y.length();
         System.out.println(biFunction.apply("hi", "Sachin"));
-
+        //in Bifunction only andThen method is
     }
 }
