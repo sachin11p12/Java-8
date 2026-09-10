8403802ffa9cef2cbf585e71fe6664768c46719e
Sachin11p12 <sachin11p12@gmail.com>
2026-09-10 21:11:06 +0530

    Implement Bifunction example

--- src/predicate/Bi_Function_java.java
+++ src/predicate/Bi_Function_java.java
@@ -1,7 +1,11 @@
 package predicate;
 
+import java.util.function.BiFunction;
+
 public class Bi_Function_java {
     public static void main(String[] args) {
+        BiFunction<String, String, Integer> biFunction = (x, y) -> x.length() + y.length();
+        System.out.println(biFunction.apply("hi", "Sachin"));
 
     }
 }
