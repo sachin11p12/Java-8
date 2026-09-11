ae82481334d7e02dbbc0f3ece2f7569287782817
Sachin11p12 <sachin11p12@gmail.com>
2026-09-11 22:27:08 +0530

    what is binary operator

---
+++ src/Unary_Binary_operator_Interface/Test_BinaryOPerator.java
@@ -0,0 +1,8 @@
+package Unary_Binary_operator_Interface;
+
+public class Test_BinaryOPerator {
+    public static void main(String[] args) {
+        // Binary operator  takes two operands of the same type and returns a result of the same type,
+
+    }
+}
