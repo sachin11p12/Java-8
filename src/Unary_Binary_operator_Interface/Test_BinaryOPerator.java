7dade1b48c665e19e3a5960d8cc819f03317d908
Sachin11p12 <sachin11p12@gmail.com>
2026-09-11 22:29:46 +0530

    Understand Binary operator

--- src/Unary_Binary_operator_Interface/Test_BinaryOPerator.java
+++ src/Unary_Binary_operator_Interface/Test_BinaryOPerator.java
@@ -5,6 +5,8 @@ import java.util.function.BinaryOperator;
 public class Test_BinaryOPerator {
     public static void main(String[] args) {
         // Binary operator  takes two operands of the same type and returns a result of the same type,
+        // It  extends BiFunction opeartore so it uses Only one  method apply()
+
         BinaryOperator<String> binaryOperator = (str1, str2) -> str1+str2;
         System.out.println(binaryOperator.apply("Sachin", " tiwari"));
 
