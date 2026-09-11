7353f041974446682e16c92115288fb08eaecc7e
Sachin11p12 <sachin11p12@gmail.com>
2026-09-11 22:28:39 +0530

    Concatenate two string with use of Binary opeartor

--- src/Unary_Binary_operator_Interface/Test_BinaryOPerator.java
+++ src/Unary_Binary_operator_Interface/Test_BinaryOPerator.java
@@ -1,8 +1,12 @@
 package Unary_Binary_operator_Interface;
 
+import java.util.function.BinaryOperator;
+
 public class Test_BinaryOPerator {
     public static void main(String[] args) {
         // Binary operator  takes two operands of the same type and returns a result of the same type,
+        BinaryOperator<String> binaryOperator = (str1, str2) -> str1+str2;
+        System.out.println(binaryOperator.apply("Sachin", " tiwari"));
 
     }
 }
