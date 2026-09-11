4bc69db6455ea284092585a743333531652069fd
Sachin11p12 <sachin11p12@gmail.com>
2026-09-11 23:04:37 +0530

    now we r calling without static method with creating objet of class

--- src/Method_Constructor_Refere/Test1.java
+++ src/Method_Constructor_Refere/Test1.java
@@ -6,14 +6,17 @@ import java.util.List;
 public class Test1 {
     //  when We need to give function as a parameter then we use method reference or Lambda expression
 
-    public static void print(String s ){
+    public  void print(String s ){ // remove static
         System.out.println(s);
     }
 
     public static void main(String[] args) {
+        Test1 test = new Test1();
+
+
         List<String> students = Arrays.asList("Sachin", "Nitin", "Utkarsh");
 //        students.forEach(x-> System.out.println(x));
-        students.forEach(Test1::print);  // :: this is method reference opeartor
+        students.forEach(test::print);  // :: this is method reference opeartor
 //        System.out.println(hello()); // here we are invoking method but using :: method ref  opeartor we are not invoking
     }
 //    private static int hello(){
