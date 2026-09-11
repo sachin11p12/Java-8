69c879842f454477fa3424d3ce1119426c8dc189
Sachin11p12 <sachin11p12@gmail.com>
2026-09-11 22:37:37 +0530

    What is Method references

---
+++ src/Method_Constructor_Refere/Test1.java
@@ -0,0 +1,13 @@
+package Method_Constructor_Refere;
+
+public class Test1 {
+    //  when We need to give function as a parameter then we use method reference or Lambda expression
+
+    public static void print(String s ){
+        System.out.println(s);
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
