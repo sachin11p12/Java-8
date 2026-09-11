20740541253c36ad59112593825a65d54bd6cb1c
Sachin11p12 <sachin11p12@gmail.com>
2026-09-11 22:22:59 +0530

    Inintialize first example of unarry operator

--- src/Unary_Binary_operator_Interface/Test.java
+++ src/Unary_Binary_operator_Interface/Test.java
@@ -1,12 +1,17 @@
 package Unary_Binary_operator_Interface;
 
 import java.util.function.Function;
+import java.util.function.UnaryOperator;
 
 public class Test {
     public static void main(String[] args) {
         Function<Integer, Integer> function = x -> x*x;
+        Function<String, String> function1 = str -> str.toLowerCase(); // here is Same input and output so we use Unary Operator
 
-        Function<String, String> function1 = str -> str.toLowerCase();
-        Function<String, Integer> function2 = str -> str.length();
+        UnaryOperator<Integer> unaryOperator = x -> x*x;
+        System.out.println(unaryOperator.apply(7));
+
+
+
     }
 }
