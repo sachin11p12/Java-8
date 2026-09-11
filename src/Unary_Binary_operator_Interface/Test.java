8226e6b93e4334ebc9fd62623bfa0f266952da42
Sachin11p12 <sachin11p12@gmail.com>
2026-09-11 22:24:17 +0530

    Inintialize string to understand how it work with strings

--- src/Unary_Binary_operator_Interface/Test.java
+++ src/Unary_Binary_operator_Interface/Test.java
@@ -8,8 +8,11 @@ public class Test {
         Function<Integer, Integer> function = x -> x*x;
         Function<String, String> function1 = str -> str.toLowerCase(); // here is Same input and output so we use Unary Operator
 
-        UnaryOperator<Integer> unaryOperator = x -> x*x;
-        System.out.println(unaryOperator.apply(7));
+//        UnaryOperator<Integer> unaryOperator = x -> x*x;
+//        System.out.println(unaryOperator.apply(7));
+
+        UnaryOperator<String> unaryOperator2 = str-> str.toLowerCase();
+        System.out.println(unaryOperator2.apply("SACHIN TIWARi"));
 
 
 
