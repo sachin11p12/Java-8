18311eae1b461ad66ee481c2d30add82a8cfde89
Sachin11p12 <sachin11p12@gmail.com>
2026-09-12 23:43:23 +0530

    use max(), min(), get in stream

--- src/stream/Stream_operationss.java
+++ src/stream/Stream_operationss.java
@@ -4,7 +4,7 @@ import java.util.stream.Stream;
 
 public class Stream_operationss {
     public static void main(String[] args) {
-        Integer integer = Stream.iterate(0, x -> x + 1).limit(101).map(x -> x/20).distinct().max((a,b) -> a-b).get();
+        Integer integer = Stream.iterate(0, x -> x + 1).limit(101).map(x -> x/20).distinct().peek(System.out::println).max((a,b) -> a-b).get();
         System.out.println(integer);
     }
 }
