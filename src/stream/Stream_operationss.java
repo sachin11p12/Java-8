2d189e9131689c2b60f32eb20d0ae28148395024
Sachin11p12 <sachin11p12@gmail.com>
2026-09-12 23:45:33 +0530

    Used count() to understand how it works with stream

--- src/stream/Stream_operationss.java
+++ src/stream/Stream_operationss.java
@@ -4,7 +4,8 @@ import java.util.stream.Stream;
 
 public class Stream_operationss {
     public static void main(String[] args) {
-        Integer integer = Stream.iterate(0, x -> x + 1).limit(101).map(x -> x/20).distinct().peek(System.out::println).max((a,b) -> a-b).get();
+//        Integer integer = Stream.iterate(0, x -> x + 1).limit(101).map(x -> x/20).distinct().peek(System.out::println).max((a,b) -> a-b).get();
+        Long integer = Stream.iterate(0, x -> x + 1).limit(101).map(x -> x/20).distinct().count();  // it use Long
         System.out.println(integer);
     }
 }
