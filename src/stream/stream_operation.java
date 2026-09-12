b537d581309c5ec281de155347c6d07a090b69c6
Sachin11p12 <sachin11p12@gmail.com>
2026-09-12 23:15:54 +0530

    use distinct() in stream

--- src/stream/stream_operation.java
+++ src/stream/stream_operation.java
@@ -8,9 +8,13 @@ public class stream_operation {
     public static void main(String[] args) {
         List<Integer> list = Arrays.asList(1,2,3,4,5,6,7,4,4,4,78,4,90);
         List<Integer> filterList = list.stream().filter(n -> n%2 == 0).collect(Collectors.toList());
-        System.out.println(filterList);
-        List<Integer> mappedList = filterList.stream().map(x -> x / 2).collect(Collectors.toList());
-        System.out.println(mappedList);
+//        System.out.println(filterList);
 
+        List<Integer> mappedList = filterList.stream().map(x -> x / 2).collect(Collectors.toList());
+//        System.out.println(mappedList);
+
+        //in above repitition found so we don't need it then use
+        List<Integer> distinctlist = filterList.stream().map(x -> x / 2).distinct().collect(Collectors.toList());
+        System.out.println(distinctlist);
     }
 }
