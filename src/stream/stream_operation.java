4a9cf3a4e7f43e503152f2abd4b72b538eb3b6aa
Sachin11p12 <sachin11p12@gmail.com>
2026-09-12 23:07:26 +0530

    performing operation on stream

---
+++ src/stream/stream_operation.java
@@ -0,0 +1,14 @@
+package stream;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class stream_operation {
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(1,2,3,4,5,6,7);
+        List<Integer> filterList = list.stream().filter(n -> n%2 == 0).collect(Collectors.toList());
+        System.out.println(filterList);
+
+    }
+}
