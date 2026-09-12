e89b5b5dd2f1585707fd87f0186b10b0042711e0
Sachin11p12 <sachin11p12@gmail.com>
2026-09-12 23:09:31 +0530

    Understand how filter can be used in stream

--- src/stream/stream_operation.java
+++ src/stream/stream_operation.java
@@ -6,7 +6,7 @@ import java.util.stream.Collectors;
 
 public class stream_operation {
     public static void main(String[] args) {
-        List<Integer> list = Arrays.asList(1,2,3,4,5,6,7);
+        List<Integer> list = Arrays.asList(1,2,3,4,5,6,7,4,4,4,78,4,90);
         List<Integer> filterList = list.stream().filter(n -> n%2 == 0).collect(Collectors.toList());
         System.out.println(filterList);
 
