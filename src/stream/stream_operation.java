e1d8d364e6488b1c044b9612ba3a54ba32e18aa7
Sachin11p12 <sachin11p12@gmail.com>
2026-09-12 23:13:08 +0530

    use map() in stream

--- src/stream/stream_operation.java
+++ src/stream/stream_operation.java
@@ -9,6 +9,8 @@ public class stream_operation {
         List<Integer> list = Arrays.asList(1,2,3,4,5,6,7,4,4,4,78,4,90);
         List<Integer> filterList = list.stream().filter(n -> n%2 == 0).collect(Collectors.toList());
         System.out.println(filterList);
+        List<Integer> mappedList = filterList.stream().map(x -> x / 2).collect(Collectors.toList());
+        System.out.println(mappedList);
 
     }
 }
