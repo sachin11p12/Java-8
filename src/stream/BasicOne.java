5c2ecb256c438ddb5d2ede9e6885a64d56a9d260
Sachin11p12 <sachin11p12@gmail.com>
2026-09-12 20:14:27 +0530

    // Declarative approach using Stream

--- src/stream/BasicOne.java
+++ src/stream/BasicOne.java
@@ -1,5 +1,7 @@
 package stream;
 
+import java.util.Arrays;
+
 public class BasicOne {
     public static void main(String[] args) {
 
@@ -11,9 +13,10 @@ public class BasicOne {
                 sum += array[i];
             }
         }
-        System.out.println(sum);
-        // declarative approach || stream
+//        System.out.println(sum);
+        // // Declarative approach using Stream
         int[] array2 = {1,2,3,4,5,6};
-
+        int sum2 = Arrays.stream(array2).filter(n->n%2==0).sum();
+        System.out.println(sum2);
     }
 }
