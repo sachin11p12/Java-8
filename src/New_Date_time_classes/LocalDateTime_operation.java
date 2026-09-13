4971e069a699bfa0d9f1c697447b5a3c216b129c
Sachin11p12 <sachin11p12@gmail.com>
2026-09-13 13:01:06 +0530

    use LocalDateTime api to print 2026-09-13T13:00:12.130001 T is separator

---
+++ src/New_Date_time_classes/LocalDateTime_operation.java
@@ -0,0 +1,10 @@
+package New_Date_time_classes;
+
+import java.time.LocalDateTime;
+
+public class LocalDateTime_operation {
+    public static void main(String[] args) {
+        LocalDateTime now = LocalDateTime.now();
+        System.out.println(now);
+    }
+}
