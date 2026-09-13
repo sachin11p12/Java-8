a529581984b0cf02bb3a14ea6318ba63c1ffcea8
Sachin11p12 <sachin11p12@gmail.com>
2026-09-13 10:16:48 +0530

    Initialize New Date  and Time API with def adn example

---
+++ src/New_Date_time_classes/Basics.java
@@ -0,0 +1,26 @@
+package New_Date_time_classes;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.Month;
+
+public class Basics {
+    public static void main(String[] args) {
+//        The legacy Date and Calendar classes had several design issues, such as mutability, confusing APIs,
+//        zero-based months in Calendar, and poor date/time-zone handling. They were also difficult to work with and prone to errors.
+//        To overcome these limitations, Java 8 introduced the java.time Date and Time API, which provides immutable, thread-safe,
+//        and easier-to-use classes.
+
+        LocalDate now = LocalDate.now();
+        System.out.println(now);
+        LocalDate myDob = LocalDate.of(2001, 12, 12);
+
+        int dayofMonth = now.getDayOfMonth();
+        System.out.println(dayofMonth);
+        Month month = now.getMonth();
+        int year = now.getYear();
+        System.out.println(month);
+        System.out.println(year);
+
+    }
+}
