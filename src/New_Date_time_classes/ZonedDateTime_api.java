5cca7ab4317cb44e9f170eb11a67f41659b18d52
Sachin11p12 <sachin11p12@gmail.com>
2026-09-13 13:08:17 +0530

    print all available  zone with use of ZoneId

--- src/New_Date_time_classes/ZonedDateTime_api.java
+++ src/New_Date_time_classes/ZonedDateTime_api.java
@@ -1,13 +1,18 @@
 package New_Date_time_classes;
 
+import java.time.ZoneId;
 import java.time.ZonedDateTime;
+import java.util.Set;
 
 public class ZonedDateTime_api {
     public static void main(String[] args) {
         // it is also same like LocalDateTime and UTC(Coordinated Universal Time.) Zoned time and Date
 
         ZonedDateTime zone = ZonedDateTime.now();
-        System.out.println(zone);
+//        System.out.println(zone);
+        Set<String> availableZoneIds = ZoneId.getAvailableZoneIds();
+
+        System.out.println(availableZoneIds);
 
     }
 }
