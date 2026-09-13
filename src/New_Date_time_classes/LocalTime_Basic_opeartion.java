62acc1037d58a72c8d6caf6653d160b4e1be1bdd
Sachin11p12 <sachin11p12@gmail.com>
2026-09-13 12:54:27 +0530

    how LocalTime works and performing different operation

---
+++ src/New_Date_time_classes/LocalTime_Basic_opeartion.java
@@ -0,0 +1,11 @@
+package New_Date_time_classes;
+
+import java.time.LocalTime;
+
+public class LocalTime_Basic_opeartion {
+    public static void main(String[] args) {
+        LocalTime now = LocalTime.now();
+        System.out.println(now);
+
+    }
+}
