81cdf8cd828bc34eefa0e116762cfc7f4796a73f
Sachin11p12 <sachin11p12@gmail.com>
2026-09-13 13:06:43 +0530

    use zoned date time to print now zone

---
+++ src/New_Date_time_classes/ZonedDateTime_api.java
@@ -0,0 +1,13 @@
+package New_Date_time_classes;
+
+import java.time.ZonedDateTime;
+
+public class ZonedDateTime_api {
+    public static void main(String[] args) {
+        // it is also same like LocalDateTime and UTC(Coordinated Universal Time.) Zoned time and Date
+
+        ZonedDateTime zone = ZonedDateTime.now();
+        System.out.println(zone);
+
+    }
+}
