28fc6af716dc62aee7dc6a3a6bb6fd622cfbcb47
Sachin11p12 <sachin11p12@gmail.com>
2026-09-13 13:20:06 +0530

    it print current mili second from 1jajn 1970 utc

---
+++ src/New_Date_time_classes/Instant_Date_time_Api.java
@@ -0,0 +1,10 @@
+package New_Date_time_classes;
+
+public class Instant_Date_time_Api {
+    public static void main(String[] args) {
+        // it print current
+        long currentTimeMillis = System.currentTimeMillis();
+        System.out.println(currentTimeMillis);
+
+    }
+}
