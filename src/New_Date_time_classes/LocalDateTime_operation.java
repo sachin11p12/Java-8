56bd0b348a3c7eac888a017078ebc64ae5657c89
Sachin11p12 <sachin11p12@gmail.com>
2026-09-13 13:03:16 +0530

    Understnd how localDateTime works and their operations

--- src/New_Date_time_classes/LocalDateTime_operation.java
+++ src/New_Date_time_classes/LocalDateTime_operation.java
@@ -6,5 +6,6 @@ public class LocalDateTime_operation {
     public static void main(String[] args) {
         LocalDateTime now = LocalDateTime.now();
         System.out.println(now);
+        // there are multiple method in this also same like date and time
     }
 }
