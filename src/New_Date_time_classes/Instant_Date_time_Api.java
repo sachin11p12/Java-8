10733e93fe7ef06b0a0c084c27ce85ccd6b8361d
Sachin11p12 <sachin11p12@gmail.com>
2026-09-14 21:13:31 +0530

    Instant in DateTime api

--- src/New_Date_time_classes/Instant_Date_time_Api.java
+++ src/New_Date_time_classes/Instant_Date_time_Api.java
@@ -1,10 +1,14 @@
 package New_Date_time_classes;
 
+import java.time.Instant;
+
 public class Instant_Date_time_Api {
     public static void main(String[] args) {
         // it print current
         long currentTimeMillis = System.currentTimeMillis();
         System.out.println(currentTimeMillis);
+        Instant now = Instant.now();
+        System.out.println(now);
 
     }
 }
