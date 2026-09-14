d5d84f2d35cb4e7820c382e2f7c53f4e9a43bb5c
Sachin11p12 <sachin11p12@gmail.com>
2026-09-14 21:16:09 +0530

    understand how zone id worked with instant

--- src/New_Date_time_classes/Instant_Date_time_Api.java
+++ src/New_Date_time_classes/Instant_Date_time_Api.java
@@ -1,6 +1,8 @@
 package New_Date_time_classes;
 
 import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 
 public class Instant_Date_time_Api {
     public static void main(String[] args) {
@@ -8,7 +10,9 @@ public class Instant_Date_time_Api {
         long currentTimeMillis = System.currentTimeMillis();
         System.out.println(currentTimeMillis);
         Instant now = Instant.now();
-        System.out.println(now);
+//        System.out.println(now);
 
+        ZonedDateTime zonedDateTime = now.atZone(ZoneId.of("Asia/Kolkata"));
+        System.out.println(zonedDateTime);
     }
 }
